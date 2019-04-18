a752cff353cee91d1c1ef6670bff8f5d73335978
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-04-18 17:59:01 +0700

    Move the Vm code into it's own crate

---
+++ src/lib.rs
@@ -0,0 +1,112 @@
+use std::io::{self, Read, Write};
+
+/// Maximum length of the Memmory size of the VM
+const MAX_LEN: usize = 30_000; // recommended size
+
+/// Brainfuck VM
+pub struct Vm {
+    /// Pointer for the `<` and `>` instructions
+    p: usize,
+    /// Memory location of the data for the pointer
+    mem: Vec<u8>,
+    /// Return stack for holding the jump location in the `[` and `]` instruction pair
+    rstack: Vec<usize>,
+}
+
+impl Vm {
+    /// Create a new brainfuck VM
+    pub fn new() -> Vm {
+        Vm {
+            p: 0,
+            mem: vec![0; MAX_LEN],
+            rstack: Vec::new(),
+        }
+    }
+
+    /// Move the pointer to the right, wraps around in case of overflow
+    fn right(&mut self) {
+        self.p = (self.p + 1) % MAX_LEN;
+    }
+
+    /// Move the pointer to the left, wraps around in case of overflow
+    fn left(&mut self) {
+        // prevent underflow, provided usize max >= 2 * 30_000
+        self.p = (self.p + MAX_LEN - 1) % MAX_LEN;
+    }
+
+    /// Add one to the currently pointed value
+    fn inc(&mut self) {
+        self.mem[self.p] = self.mem[self.p].wrapping_add(1);
+    }
+
+    /// Substract one from the currently pointed value
+    fn dec(&mut self) {
+        self.mem[self.p] = self.mem[self.p].wrapping_sub(1);
+    }
+
+    /// Output currently pointed value into stdout
+    fn put(&self) {
+        let buff = [self.mem[self.p]];
+        match io::stdout().write(&buff) {
+            Ok(_) => (),
+            Err(e) => eprintln!("IO Error in `put`: {}", e),
+        }
+    }
+
+    /// Get one byte from the input and put it in the currently pointed value
+    fn get(&mut self) {
+        let mut buff = [0; 1];
+        match io::stdin().read(&mut buff) {
+            Ok(0) => self.mem[self.p] = 0,
+            Ok(_) => self.mem[self.p] = buff[0],
+            Err(e) => eprintln!("IO Error in `get`: {}", e),
+        }
+    }
+
+    /// Check if currently pointed value is zero
+    fn is_zero(&self) -> bool {
+        self.mem[self.p] == 0
+    }
+
+    /// Run a brainfuck script, in case of unmatched bracket, return the unmatched location
+    pub fn run(&mut self, script: &[u8]) -> Result<(), usize> {
+        let mut cursor = 0;
+        while cursor < script.len() {
+            let byte = script[cursor];
+            let mut branching = false;
+            match byte {
+                b'<' => self.left(),
+                b'>' => self.right(),
+                b'+' => self.inc(),
+                b'-' => self.dec(),
+                b'.' => self.put(),
+                b',' => self.get(),
+                b'[' => self.rstack.push(cursor+1),
+                b']' => branching = true,
+                _ => (),
+            }
+            if branching {
+                let jump = !self.is_zero();
+                match (self.rstack.last(), jump) {
+                    // loop unfinished, jump back
+                    (Some(&n), true) => {
+                        cursor = n;
+                    }
+                    // loop finished, continue to next byte
+                    (Some(_), false) => {
+                        self.rstack.pop(); // clear this jump
+                        cursor += 1;
+                    }
+                    (None, _) => return Err(cursor),
+                }
+            } else {
+                cursor += 1;
+            }
+        }
+        if self.rstack.is_empty() {
+            Ok(())
+        } else {
+            Err(self.rstack[self.rstack.len()-1])
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,18 @@
-use std::io::{self, Read, Write};
+extern crate brainf;
+
+use std::io::Read;
 use std::env;
 use std::process;
 use std::fs::File;
 
-// recommended size
-const MAX_LEN: usize = 30_000;
-
-struct Vm {
-    p: usize,
-    mem: Vec<u8>,
-    rstack: Vec<usize>,
-}
-
-impl Vm {
-    fn new() -> Vm {
-        Vm {
-            p: 0,
-            mem: vec![0; MAX_LEN],
-            rstack: Vec::new(),
-        }
-    }
-    fn right(&mut self) {
-        self.p = (self.p + 1) % MAX_LEN;
-    }
-    fn left(&mut self) {
-        // prevent underflow, provided usize max >= 2 * 30_000
-        self.p = (self.p + MAX_LEN - 1) % MAX_LEN;
-    }
-    fn inc(&mut self) {
-        self.mem[self.p] = self.mem[self.p].wrapping_add(1);
-    }
-    fn dec(&mut self) {
-        self.mem[self.p] = self.mem[self.p].wrapping_sub(1);
-    }
-    fn put(&self) {
-        let buff = [self.mem[self.p]];
-        match io::stdout().write(&buff) {
-            Ok(_) => (),
-            Err(e) => eprintln!("IO Error in `put`: {}", e),
-        }
-    }
-    fn get(&mut self) {
-        let mut buff = [0; 1];
-        match io::stdin().read(&mut buff) {
-            Ok(0) => self.mem[self.p] = 0,
-            Ok(_) => self.mem[self.p] = buff[0],
-            Err(e) => eprintln!("IO Error in `get`: {}", e),
-        }
-    }
-    fn is_zero(&self) -> bool {
-        self.mem[self.p] == 0
-    }
-    fn run(&mut self, script: &[u8]) -> Result<(), usize> {
-        let mut cursor = 0;
-        while cursor < script.len() {
-            let byte = script[cursor];
-            let mut branching = false;
-            match byte {
-                b'<' => self.left(),
-                b'>' => self.right(),
-                b'+' => self.inc(),
-                b'-' => self.dec(),
-                b'.' => self.put(),
-                b',' => self.get(),
-                b'[' => self.rstack.push(cursor+1),
-                b']' => branching = true,
-                _ => (),
-            }
-            if branching {
-                let jump = !self.is_zero();
-                match (self.rstack.last(), jump) {
-                    // loop unfinished, jump back
-                    (Some(&n), true) => {
-                        cursor = n;
-                    }
-                    // loop finished, continue to next byte
-                    (Some(_), false) => {
-                        self.rstack.pop(); // clear this jump
-                        cursor += 1;
-                    }
-                    (None, _) => return Err(cursor),
-                }
-            } else {
-                cursor += 1;
-            }
-        }
-        if self.rstack.is_empty() {
-            Ok(())
-        } else {
-            Err(self.rstack[self.rstack.len()-1])
-        }
-    }
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!(r#"brainf v0.1: brainfuck interpreter.
+        eprintln!(r#"brainf v0.1: brainfuck interpreter.
 
 USAGE: brainf <file>
 
-<file> brainfuck program to run."#);
+* <file> : brainfuck program to run."#);
         process::exit(1);
     }
 
@@ -121,7 +33,8 @@ USAGE: brainf <file>
         }
     }
 
-    let mut vm = Vm::new();
+    let mut vm = brainf::Vm::new();
+
     match vm.run(&script) {
         Err(pos) => {
             eprintln!("Error, unmatched bracket at {}", pos);
