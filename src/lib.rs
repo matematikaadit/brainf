fd753d30660fd641ebcc4f53114e345f8f5636fa
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-04-20 11:00:30 +0700

    Small note about the test

--- src/lib.rs
+++ src/lib.rs
@@ -182,6 +182,7 @@ mod test {
     #[test]
     fn adding_two_one_digit_number() {
         let script = b",>++++[->++++[-<<--->>]<],>++++[->++++[-<<--->>]<]<[-<+>]++++[->++++[-<<+++>>]<]<.";
+        // Note that this only works with a single digit result
 
         // 2 + 6 == 8
         let mut test_io = TestIO::new(vec![b'2', b'6']);
