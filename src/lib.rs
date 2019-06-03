9fccabaa5d984ddef4732b6cf056d9b16c070d10
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-06-03 16:31:30 +0700

    Simpler implementation with test and a working main()

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,31 @@ use std::io::Read;
 use std::num::Wrapping;
 
 
+/// Maximum length for the brainfuck program's memory
+const MAXLEN: usize = 30_000; // recommended size
+
+
+/// Possible Error when evaluating the brainfuck program
 pub enum Error {
+    /// Unmatched right bracket (`]`)
     RBrac(usize),
+    /// Unmatched left brackets ('[')
     LBrac(Vec<usize>),
+    /// Input error
     Input(io::Error),
+    /// Output error
     Output(io::Error),
 }
 
 
+/// Evaluating a brainfuck script with input/output manually provided to the function
 pub fn eval<R, W>(src: &[u8], input: &mut R, output: &mut W) -> Result<(), Error>
 where R: Read,
       W: Write {
     let mut iptr = 0;
     let mut rstack = Vec::new();
 
-    const LEN: usize = 30_000;
-    let mut data = vec![Wrapping(0u8); LEN];
+    let mut data = vec![Wrapping(0u8); MAXLEN];
     let mut dptr = 0;
 
     loop {
@@ -36,11 +45,11 @@ where R: Read,
             }
             Some(b'<') => {
                 // avoiding underflow
-                dptr = dptr.checked_sub(1).unwrap_or_else(|| LEN);
+                dptr = dptr.checked_sub(1).unwrap_or_else(|| MAXLEN);
                 iptr += 1;
             }
             Some(b'>') => {
-                dptr = (dptr + 1) % LEN;
+                dptr = (dptr + 1) % MAXLEN;
                 iptr += 1;
             }
             Some(b',') => {
@@ -86,188 +95,27 @@ where R: Read,
 }
 
 
-
-// old implementation
-
-
-/// Trait for our VM's Input/Output
-pub trait IO {
-    /// Get one byte from the input
-    fn get(&mut self) -> u8;
-    /// Put one byte into the output
-    fn put(&mut self, byte: u8);
+/// Evaluating brainfuck script with the default stdin/stdout
+pub fn eval_with_stdio(src: &[u8]) -> Result<(), Error> {
+    eval(src, &mut io::stdin(), &mut io::stdout())
 }
 
-/// Input/Output using io::stdin() and io::stdout()
-pub struct StdIO;
-
-impl IO for StdIO {
-    fn get(&mut self) -> u8 {
-        let mut buff = [0; 1];
-        match io::stdin().read(&mut buff) {
-            Ok(0) => 0,
-            Ok(_) => buff[0],
-            Err(e) => {
-                // print error, but return 0 (null)
-                eprintln!("IO Error in `get`: {}", e);
-                0
-            }
-        }
-    }
-    fn put(&mut self, byte: u8) {
-        match io::stdout().write(&[byte]) {
-            Ok(_) => (),
-            Err(e) => eprintln!("IO Error in `put`: {}", e),
-        }
-    }
-}
-
-/// Maximum length of VM's memory size
-const MAX_LEN: usize = 30_000; // recommended size
-
-/// Brainfuck VM
-pub struct Vm<T> {
-    /// Pointer to be manipulated by the instructions
-    p: usize,
-    /// Memory for holding the data
-    mem: Vec<u8>,
-    /// Return stack for holding the jump location in the `[` and `]` instruction pair
-    rstack: Vec<usize>,
-    /// Input/Output for the `.` and `,` instruction
-    io: T,
-}
-
-impl<T> Vm<T> where T: IO {
-    /// Create a new brainfuck VM
-    pub fn new(io: T) -> Vm<T> {
-        Vm {
-            p: 0,
-            mem: vec![0; MAX_LEN],
-            rstack: Vec::new(),
-            io: io,
-        }
-    }
-
-    /// Move the pointer to the right, wraps around in case of overflow
-    fn right(&mut self) {
-        self.p = (self.p + 1) % MAX_LEN;
-    }
-
-    /// Move the pointer to the left, wraps around in case of overflow
-    fn left(&mut self) {
-        // prevent underflow, provided usize max >= 2 * 30_000
-        self.p = (self.p + MAX_LEN - 1) % MAX_LEN;
-    }
-
-    /// Add one to the currently pointed value
-    fn inc(&mut self) {
-        self.mem[self.p] = self.mem[self.p].wrapping_add(1);
-    }
-
-    /// Substract one from the currently pointed value
-    fn dec(&mut self) {
-        self.mem[self.p] = self.mem[self.p].wrapping_sub(1);
-    }
-    /// Check if currently pointed value is zero
-    fn is_zero(&self) -> bool {
-        self.mem[self.p] == 0
-    }
-
-    // TODO: maybe create custom error type and return it instead?
-    // also how about adding error type for io instruction in case they fail?
-    /// Run a brainfuck script, in case of unmatched bracket, return the unmatched location
-    pub fn run(&mut self, script: &[u8]) -> Result<(), usize> {
-        let mut cursor = 0;
-        while cursor < script.len() {
-            let byte = script[cursor];
-            let mut branching = false;
-            match byte {
-                b'<' => self.left(),
-                b'>' => self.right(),
-                b'+' => self.inc(),
-                b'-' => self.dec(),
-                b'.' => self.io.put(self.mem[self.p]),
-                b',' => self.mem[self.p] = self.io.get(),
-                b'[' => self.rstack.push(cursor+1),
-                b']' => branching = true,
-                _ => (),
-            }
-            if branching {
-                let jump = !self.is_zero();
-                match (self.rstack.last(), jump) {
-                    // loop unfinished, jump back
-                    (Some(&n), true) => {
-                        cursor = n;
-                    }
-                    // loop finished, continue to next byte
-                    (Some(_), false) => {
-                        self.rstack.pop(); // clear this jump
-                        cursor += 1;
-                    }
-                    (None, _) => return Err(cursor),
-                }
-            } else {
-                cursor += 1;
-            }
-        }
-        if self.rstack.is_empty() {
-            Ok(())
-        } else {
-            Err(self.rstack[self.rstack.len()-1])
-        }
-    }
-}
-
-/// Create default VM using IO from StdIO
-pub fn default_vm() -> Vm<StdIO> {
-    Vm::new(StdIO)
-}
 
 #[cfg(test)]
 mod test {
-    use crate::IO;
-    use crate::Vm;
-    use std::collections::VecDeque;
-    use std::iter::{IntoIterator, FromIterator};
-
-    struct TestIO {
-        input: VecDeque<u8>,
-        output: Vec<u8>,
-    }
-
-    impl TestIO {
-        fn new(input: impl IntoIterator<Item=u8>) -> Self {
-            TestIO {
-                input: VecDeque::from_iter(input),
-                output: Vec::new(),
-            }
-        }
-    }
-
-    impl IO for &mut TestIO {
-        fn get(&mut self) -> u8 {
-            match self.input.pop_front() {
-                Some(n) => n,
-                None => 0,
-            }
-        }
-        fn put(&mut self, byte: u8) {
-            self.output.push(byte)
-        }
-    }
+    use super::eval;
+    use std::io;
+    use std::io::Cursor;
 
     #[test]
     fn test_hello_world() {
         // taken from wikipedia
         let hello_world = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-        let mut test_io = TestIO::new(Vec::new());
-        {
-            // on scope so we can get back the mutably borrowed test_io when it finished
-            let mut vm = Vm::new(&mut test_io);
-            assert_eq!(vm.run(hello_world), Ok(()));
-        }
-        assert_eq!(test_io.output, b"Hello World!\n");
+        let mut output = Vec::new();
+        assert!(eval(hello_world, &mut io::empty(), &mut output).is_ok());
+        assert_eq!(&output, b"Hello World!\n");
     }
+
 
     #[test]
     fn adding_two_one_digit_number() {
@@ -275,19 +123,15 @@ mod test {
         // Note that this only works with a single digit result
 
         // 2 + 6 == 8
-        let mut test_io = TestIO::new(vec![b'2', b'6']);
-        {
-            let mut vm = Vm::new(&mut test_io);
-            assert_eq!(vm.run(&script[..]), Ok(()));
-        }
-        assert_eq!(test_io.output, b"8");
+        let mut input = Cursor::new(b"26");
+        let mut output = Vec::new();
+        assert!(eval(script, &mut input, &mut output).is_ok());
+        assert_eq!(&output, b"8");
 
         // 4 + 3 == 7
-        let mut test_io = TestIO::new(vec![b'4', b'3']);
-        {
-            let mut vm = Vm::new(&mut test_io);
-            assert_eq!(vm.run(&script[..]), Ok(()));
-        }
-        assert_eq!(test_io.output, b"7");
+        let mut input = Cursor::new(b"43");
+        output.clear();
+        assert!(eval(script, &mut input, &mut output).is_ok());
+        assert_eq!(&output, b"7");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-extern crate brainf;
-
 use std::io::Read;
 use std::env;
-use std::process;
+use std::process::exit;
 use std::fs::File;
 
+use brainf::Error;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!(r#"brainf v0.1: brainfuck interpreter.
+    let mut args = env::args().skip(1); // skip args[0] (program name)
+    let script_name = match args.next() {
+        Some(s) => s,
+        None => {
+            usage();
+            exit(1);
+        }
+    };
 
-USAGE: brainf <file>
-
-* <file> : brainfuck program to run."#);
-        process::exit(1);
-    }
-
-    let mut file = match File::open(&args[1]) {
+    let mut file = match File::open(script_name) {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("Error opening file: {}", e);
-            process::exit(1)
+            eprintln!("Error opening script: {}", e);
+            exit(1)
         }
     };
 
@@ -28,18 +27,42 @@ USAGE: brainf <file>
     match file.read_to_end(&mut script) {
         Ok(_) => (),
         Err(e) => {
-            eprintln!("Error reading file: {}", e);
-            process::exit(1);
+            eprintln!("Error reading script: {}", e);
+            exit(1);
         }
     }
 
-    let mut vm = brainf::default_vm();
-
-    match vm.run(&script) {
-        Err(pos) => {
-            eprintln!("Error, unmatched bracket at {}", pos);
-            process::exit(1);
+    match brainf::eval_with_stdio(&script) {
+        Err(Error::RBrac(pos)) => {
+            eprintln!("Error, unmatched ] at position: {}", pos);
+            exit(1);
+        }
+        Err(Error::LBrac(positions)) => {
+            eprint!("Error, unmatched [ at position: ");
+            for pos in positions {
+                eprint!("{}, ", pos);
+            }
+            eprintln!();
+            exit(1);
+        }
+        Err(Error::Input(e)) => {
+            eprintln!("Error reading from stdin: {}", e);
+            exit(1);
+        }
+        Err(Error::Output(e)) => {
+            eprintln!("Error writing to stdout: {}", e); // and hoping this eprintln also not error
+            exit(1);
         }
         Ok(()) => (),
     }
+}
+
+fn usage() {
+    eprintln!(
+r#"brainf v0.2: brainfuck interpreter.
+
+USAGE: brainf <file>
+
+* <file> : brainfuck program to run.
+"#);
 }
